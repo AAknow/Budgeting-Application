3d1a9dda8afe8bc962e86ae04d8b33f60728f504
AAknow <knowlesaa77@gmail.com>
2026-06-07 21:36:45 -0500

    add sequelize and body-parser. create users, expenses, and goals tables

--- server/index.js
+++ server/index.js
@@ -1,6 +1,46 @@
 // Express.js
 const express = require('express');
+const bodyParser = require('body-parser');
+const { Sequelize, Model, DataTypes } = require('sequelize');
+
 const app = express();
+
+// Create Sequelize instance
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './database.sqlite'
+});
+
+// Define User model
+class User extends Model {}
+User.init({
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
+}, { sequelize, modelName: 'users' });
+
+// Define Expenses model
+class Expenses extends Model {}
+User.init({
+    expense: DataTypes.STRING,
+    amount: DataTypes.FLOAT,
+    date: DataTypes.DATE
+}, { sequelize, modelName: 'expenses' });
+
+// Define Goals model
+class Goals extends Model {}
+User.init({
+    expense: DataTypes.STRING,
+    amount: DataTypes.FLOAT,
+    date: DataTypes.DATE
+}, { sequelize, modelName: 'goals' });
+
+// Sync models with database
+sequelize.sync();
+
+// Middleware for parsing request body
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Routes
 const userRouter = require("./routes/userRouter"); 
