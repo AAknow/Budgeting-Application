67601411d11226afa02cadb2a3b86327d8cb3e57
AAknow <knowlesaa77@gmail.com>
2026-06-15 21:28:18 -0500

    add controller dependency for getExpenseById, replaced get expenseId with controller logic

--- server/routes/expenseRouter.js
+++ server/routes/expenseRouter.js
@@ -1,11 +1,10 @@
 const { Router } = require("express"); 
+const { getExpenseById } = require('../controllers/expenseController');
+
 const expenseRouter = Router();
- 
+
 expenseRouter.get("/", (req, res) => res.send("All Expenses")); 
 
-expenseRouter.get("/:expenseId", (req, res) => { 
-    const { expenseId } = req.params; 
-    res.send(`Expense ID: ${expenseId}`); 
-}); 
+expenseRouter.get("/:expenseId", getExpenseById);
 
 module.exports = expenseRouter;
