d2df6c2b608c30ae24d0b69898288b6667917a4b
AAknow <knowlesaa77@gmail.com>
2026-06-15 19:57:37 -0500

    add controller dependency for getUserById, replaced get userId with controller logic

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,11 +1,18 @@
 const { Router } = require("express"); 
+const { getUserById } = require('../controllers/userController');
+
 const userRouter = Router();
 
 userRouter.get("/", (req, res) => res.send("All Users")); 
 
-userRouter.get("/:userId", (req, res) => { 
-    const { userId } = req.params; 
-    res.send(`User ID: ${userId}`); 
-}); 
+userRouter.get("/:userId", getUserById);
+
+//userRouter.get("/", (req, res) => res.send("All Users")); 
+
+//userRouter.get("/:userId", (req, res) => { 
+//    const { userId } = req.params; 
+//    res.send(`User ID: ${userId}`); 
+//}); 
+
 
 module.exports = userRouter;
