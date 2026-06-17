aaaad289b1951e1ba9563682c165b2b5ddbff0cf
AAknow <knowlesaa77@gmail.com>
2026-06-16 20:49:59 -0500

    remove dummy text and add app title, username form, password form, and login button html

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+
+        <header>Budgeting Application</header>
+
+        <form>
+          <input type="text"></input>
+        </form>
+
+        <form>
+          <input type="text"></input>
+        </form>
+        <button>Login</button>
+
+      </div>
     </div>
   );
 }
