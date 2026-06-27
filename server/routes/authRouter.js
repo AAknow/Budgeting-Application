b3bbf3b53fac78905e17f75c86244930fc5c3595
AAknow <knowlesaa77@gmail.com>
2026-06-27 16:43:47 -0500

    create router paths for login and signup

---
+++ server/routes/authRouter.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+const { login, signup } = require("../controllers/authController");
+
+const authRouter = Router();
+
+authRouter.post("/login", login);
+
+authRouter.post("/signup", signup);
+
+module.exports = authRouter;
